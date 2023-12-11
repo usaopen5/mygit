4abcdc7a95faf98b752b5a484ccf2d267b54ff35
usaopen5 <82417052+usaopen5@users.noreply.github.com>
2023-12-12 01:25:06 +0900

    Update 1929 소수 구하기.cpp

--- math/1929 소수 구하기.cpp
+++ math/1929 소수 구하기.cpp
@@ -14,23 +14,23 @@ int main()
 	cin >> M >> N;
 	vector <int>v;
 
-	for (int i = M; i <= N; i++)
+	for (int i = M; i <= N; i++) 
 	{
-		int isPrime = 0;
-		if (i == 1)
+		int isPrime = 0; // 0이면 소수
+		if (i == 1) // 1은 소수가 아님
 		{
 			isPrime++;
-			continue;
+			continue; // 하던거 마저 하쇼
 		}
-		for (int j = 2; j <=sqrt(i); j++)
+		for (int j = 2; j <=sqrt(i); j++) 
 		{
-			if (i % j == 0)
+			if (i % j == 0) // 약수가 있으면
 			{
 				isPrime++;
-				break;
+				break; // 하나라도 약수가 있는 순간 소수 조건 탈락
 			}
 		}
-		if (isPrime == 0)
+		if (isPrime == 0) // 소수는 벡터에 저장
 		{
 			v.push_back(i);
 		}
